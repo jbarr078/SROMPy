ad80866a0ab3963f9a8b6aa4ff72146648cbf0df
Juan Barrientos <juan.m.barrientos@nasa.gov>
2019-02-22 10:58:13 -0500

    Updated import and implemented optimization params test

--- tests/srom/test_SROMSimulator.py
+++ tests/srom/test_SROMSimulator.py
@@ -8,9 +8,12 @@ if 'PYTHONPATH' not in os.environ:
 
     sys.path.insert(0, base_path)
 
+sys.path.insert(0, '/home/jmbarrie/Documents/DevOps/SROMPy/tests/test_scripts_data')
+
 from SROMPy.target import BetaRandomVariable, SampleRandomVector
-from SROMPy.srom import SROM, SROMSimulator, SROMSurrogate, FiniteDifference as FD
-from tests.testing_scripts import SpringMassModel
+from SROMPy.srom import SROM, SROMSimulator, SROMSurrogate, \
+                        FiniteDifference as FD
+from spring_mass_model import SpringMassModel
 
 @pytest.fixture
 def beta_random_variable():
@@ -49,17 +52,17 @@ def test_simulator_init_exception_for_invalid_parameters(beta_random_variable,
         SROMSimulator(1, spring_model_fixture)
 
     with pytest.raises(TypeError):
-        SROMSimulator(beta_random_variable, "Not A Proper Model")
+        SROMSimulator(beta_random_variable, 'Not A Proper Model')
 
 def test_simulate_exception_for_invalid_parameters(srom_simulator_fixture):
     with pytest.raises(TypeError):
-        srom_simulator_fixture.simulate(10.5, "PWC", 1e-12)
+        srom_simulator_fixture.simulate(10.5, 'PWC', 1e-12)
 
     with pytest.raises(ValueError):
-        srom_simulator_fixture.simulate(10, "no", 1e-12)
+        srom_simulator_fixture.simulate(10, 'no', 1e-12)
 
     with pytest.raises(TypeError):
-        srom_simulator_fixture.simulate(10, "PWL")
+        srom_simulator_fixture.simulate(10, 'PWL')
 
 
 def test_simulate_pwc_spring_mass(srom_simulator_fixture):
@@ -68,7 +71,7 @@ def test_simulate_pwc_spring_mass(srom_simulator_fixture):
     test_scripts_data/generate_srom_sim_ref_solution.py
     '''
 
-    pwc_surrogate = srom_simulator_fixture.simulate(10, "PWC")
+    pwc_surrogate = srom_simulator_fixture.simulate(10, 'PWC')
 
     mean_pwc = pwc_surrogate.compute_moments(1)[0][0]
     mean_reference = 12.385393457327542
@@ -81,13 +84,34 @@ def test_simulate_pwl_spring_mass(srom_simulator_fixture):
     test_scripts_data/generate_srom_sim_ref_solution.py
     '''
 
-    pwl_surrogate = srom_simulator_fixture.simulate(10, "PWL", 1e-12)
+    pwl_surrogate = srom_simulator_fixture.simulate(10, 'PWL', 1e-12)
 
     output_pwl = pwl_surrogate.sample(np.array([2]))
     output_ref = np.array([[14.69958116]])
 
     assert np.isclose(output_pwl, output_ref)
 
+def test_optimization_parameters(srom_simulator_fixture):
+    srom_simulator_fixture.set_optimization_parameters(weights=[2,3,5],
+                                                       num_tests=750,
+                                                       error='MEAN',
+                                                       max_moment=7,
+                                                       cdf_grid_pts=75,
+                                                       tolerance=.01,
+                                                       options={'maxiter': 800,
+                                                                'disp': False},
+                                                       method='CG',
+                                                       joint_opt=False)
+    
+    assert srom_simulator_fixture._weights == [2,3,5]
+    assert srom_simulator_fixture._num_test_samples == 750
+    assert srom_simulator_fixture._error == 'MEAN'
+    assert srom_simulator_fixture._max_moment == 7
+    assert srom_simulator_fixture._cdf_grid_pts == 75
+    assert srom_simulator_fixture._tolerance == .01
+    assert srom_simulator_fixture._options == {'maxiter': 800, 'disp': False}
+    assert srom_simulator_fixture._method == 'CG'
+    assert srom_simulator_fixture._joint_opt == False 
 
 def test_evaluate_model_for_samples_return_type(srom_simulator_fixture,
                                                 srom_base_fixture):
@@ -120,11 +144,11 @@ def test_compute_pwl_gradient_return_type(srom_simulator_fixture,
 def test_simulate_return_type(srom_simulator_fixture):
     test_pwc_surrogate = \
         srom_simulator_fixture.simulate(srom_size=10,
-                                        surrogate_type="PWC")
+                                        surrogate_type='PWC')
 
     test_pwl_surrogate = \
         srom_simulator_fixture.simulate(srom_size=10,
-                                        surrogate_type="PWL",
+                                        surrogate_type='PWL',
                                         pwl_step_size=1e-12)
 
     assert isinstance(test_pwc_surrogate, SROMSurrogate)
